ff922640c22bd9e1157bac4c15431df8a0b11aa3
Pramot674259011 <674259011@webmail.npru.ac.th>
2024-07-30 11:16:38 +0700

    first commit

---
+++ num1.java
@@ -0,0 +1,10 @@
+public class num1  
+{
+
+   public static void main(String[] args) {
+    
+    int fahrenheit =80;
+    double celsius = (5.0/9.0) * (fahrenheit-32);   
+   System.out.println(celsius); 
+} 
+}
